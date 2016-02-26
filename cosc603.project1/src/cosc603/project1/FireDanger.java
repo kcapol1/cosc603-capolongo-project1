8c715deed0535af1516fa4be6ff4f05cf5dba1fa
Ken Capolongo <kcapol1@students.towson.edu>
2016-02-26 00:52:38 -0500

    Added initialization

--- cosc603.project1/src/cosc603/project1/FireDanger.java
+++ cosc603.project1/src/cosc603/project1/FireDanger.java
@@ -5,35 +5,58 @@ public class FireDanger {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-//		Start
-//		Initialize subroutine
-//		FFM=99
-//		ADMF=99
-//		DF=0
-//		FLOAD=0
-//		A(1)=-0.185900
-//		A(2)=-0.85900
-//		A(3)=-0.05966
-//		A(4)=-0.07737
-//		B(1)=30.0
-//		B(2)=19.2
-//		B(3)=13.8
-//		B(4)=22.5
-//		C(1)=4.5
-//		C(2)=12.5
-//		C(3)=27.5
-//		D(1)=16.0
-//		D(2)=10.0
-//		D(3)=7.0
-//		D(4)=5.0
-//		D(5)=4.0
-//		D(6)=3.0
+//		Initialize
+		int ISNOW = 0;
+		double PRECIP = 0.1;
+		double DRY = 0;
+		double WET = 0;
+		double WIND = 14;
+		double IHERB = 0;
+		double BUO = 3;
+		double FFM = 99;
+		double ADMF = 99;
+		double DF = 0;
+		double FLOAD = 0;
+
+		// declares an array of doubles
+		double[] A;
+		double[] B;
+		double[] C;
+		double[] D;
+
+        // allocates memory for 10 doubles
+        A = new double[4];
+        B = new double[4];
+        C = new double[3];
+        D = new double[6];
+           
+        // initialize first element
+        A[0] = -0.185900;
+        // initialize second element
+        A[1] = -0.85900;
+        // and so forth
+        A[2] = -0.05966;
+        A[3] = -0.07737;
+        B[0] = 30.0;
+        B[1] = 19.2;
+        B[2] = 13.8;
+        B[3] = 22.5;
+        C[0] = 4.5;
+        C[1] = 12.5;
+        C[2] = 27.5;
+
+		D[0]=16.0;
+		D[1]=10.0;
+		D[2]=7.0;
+		D[3]=5.0;
+		D[4]=4.0;
+		D[5]=3.0;
 
 //		If ISNOW > 0 Then
 //			GRASS=0
 //			TIMBER=0
 //			If PRECIP > 0.1 then 
-//		BUO=-50.*ALOG(1.-(1.-EXP (-BUI/50.))*EXP (-1.175*(PRECIP-.1)))
+//		BUO=-50.*ALOG(1.-(1.-EXP (-BUO/50.))*EXP (-1.175*(PRECIP-.1)))
 //		If BUO < 0 then BUI = 0
 //			End If
 //		Else
