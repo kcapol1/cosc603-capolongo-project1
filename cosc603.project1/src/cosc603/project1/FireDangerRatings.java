b34034b46917288a2d0d5db9cf8e860e409c05fa
Ken Capolongo <kcapol1@students.towson.edu>
2016-02-28 17:45:55 -0500

    Added input attributes to FireDangerRatings class

--- cosc603.project1/src/cosc603/project1/FireDangerRatings.java
+++ cosc603.project1/src/cosc603/project1/FireDangerRatings.java
@@ -8,5 +8,11 @@ package cosc603.project1;
  *
  */
 public class FireDangerRatings {
-
+	private static double dryBulbTemperature_;
+	private static double wetBulbTemperature_;
+	private static boolean isSnowPresent_;
+	private	static double precipitation_;
+	private	static double windSpeed_;
+	private	static double districtHerbState_;	// The current herb state of the district
+	private	static double buildupIndex_;
 }
